861769a224f244e3fb0d9f8e5d2ac7af861e4a93
Philipp Kübler <info@pkuebler.de>
2024-01-26 22:44:53 +0100

    change format

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 class DrawingApp {
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
-  private pointofinterest: Array<{x1: number, x2: number, y1: number, y2: number, action: string}> = [];
+  private pointofinterest: Array<{ x1: number, x2: number, y1: number, y2: number, action: string }> = [];
   private action: string | undefined;
 
   private clickX: number[] = [];
@@ -24,20 +24,20 @@ class DrawingApp {
     this.canvas = canvas;
     this.context = context;
 
-	// load scene image
-	const image = new Image();
-	image.src = "scene/scene1.png";
-	image.onload = () => {
-		context.drawImage(image, 0, 0);
-	}
-	// load points of interest
-	const pointofinterest = [];
-	pointofinterest.push({x1: 0, x2: 100, y1: 0, y2: 100, action: "move"});
-	pointofinterest.push({x1: 100, x2: 200, y1: 0, y2: 100, action: "move2"});
-	pointofinterest.push({x1: 200, x2: 300, y1: 0, y2: 100, action: "move3"});
-	pointofinterest.push({x1: 300, x2: 400, y1: 0, y2: 100, action: "move4"});
+    // load scene image
+    const image = new Image();
+    image.src = "scene/scene1.png";
+    image.onload = () => {
+      context.drawImage(image, 0, 0);
+    }
+    // load points of interest
+    const pointofinterest = [];
+    pointofinterest.push({ x1: 0, x2: 100, y1: 0, y2: 100, action: "move" });
+    pointofinterest.push({ x1: 100, x2: 200, y1: 0, y2: 100, action: "move2" });
+    pointofinterest.push({ x1: 200, x2: 300, y1: 0, y2: 100, action: "move3" });
+    pointofinterest.push({ x1: 300, x2: 400, y1: 0, y2: 100, action: "move4" });
 
-	this.pointofinterest = pointofinterest;
+    this.pointofinterest = pointofinterest;
 
     this.redraw();
     this.createUserEvents();
@@ -80,12 +80,12 @@ class DrawingApp {
     mouseX -= this.canvas.offsetLeft;
     mouseY -= this.canvas.offsetTop;
 
-	console.log(mouseX, mouseY);
-	this.pointofinterest.forEach((point : {x1: number, x2: number, y1: number,y2: number, action: string}) => {
-		if(mouseX > point.x1 && mouseY > point.y1 && mouseX < point.x2 && mouseY < point.y2) {
-			console.log(point.action);
-		}
-	});
+    console.log(mouseX, mouseY);
+    this.pointofinterest.forEach((point: { x1: number, x2: number, y1: number, y2: number, action: string }) => {
+      if (mouseX > point.x1 && mouseY > point.y1 && mouseX < point.x2 && mouseY < point.y2) {
+        console.log(point.action);
+      }
+    });
 
     this.redraw();
   };
