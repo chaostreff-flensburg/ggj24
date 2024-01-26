d492aa857c2840561743dc97d8101929e008df88
Alexander Eichhorn <alex@code-with.us>
2024-01-26 21:21:58 +0100

    deno bundle test 1

--- main.ts
+++ main.ts
@@ -1,8 +1,122 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { HTMLCanvasElement } from "canvas";
+import { CanvasRenderingContext2D } from "canvas";
+class DrawingApp {
+  private canvas: HTMLCanvasElement;
+  private context: CanvasRenderingContext2D;
+  private paint: boolean | undefined;
+
+  private clickX: number[] = [];
+  private clickY: number[] = [];
+  private clickDrag: boolean[] = [];
+
+  /// <reference lib="dom" />
+  constructor() {
+    let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    let context = canvas.getContext("2d");
+    context.lineCap = "round";
+    context.lineJoin = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+
+    this.canvas = canvas;
+    this.context = context;
+
+    this.redraw();
+    this.createUserEvents();
+  }
+  private createUserEvents() {
+    let canvas = this.canvas;
+
+    canvas.addEventListener("mousedown", this.pressEventHandler);
+    canvas.addEventListener("mousemove", this.dragEventHandler);
+    canvas.addEventListener("mouseup", this.releaseEventHandler);
+    canvas.addEventListener("mouseout", this.cancelEventHandler);
+
+    canvas.addEventListener("touchstart", this.pressEventHandler);
+    canvas.addEventListener("touchmove", this.dragEventHandler);
+    canvas.addEventListener("touchend", this.releaseEventHandler);
+    canvas.addEventListener("touchcancel", this.cancelEventHandler);
+
+    document.getElementById("clear")
+      .addEventListener("click", this.clearEventHandler);
+  }
+
+  private redraw() {
+    let clickX = this.clickX;
+    let context = this.context;
+    let clickDrag = this.clickDrag;
+    let clickY = this.clickY;
+    for (let i = 0; i < clickX.length; ++i) {
+      context.beginPath();
+      if (clickDrag[i] && i) {
+        context.moveTo(clickX[i - 1], clickY[i - 1]);
+      } else {
+        context.moveTo(clickX[i] - 1, clickY[i]);
+      }
+
+      context.lineTo(clickX[i], clickY[i]);
+      context.stroke();
+    }
+    context.closePath();
+  }
+  private addClick(x: number, y: number, dragging: boolean) {
+    this.clickX.push(x);
+    this.clickY.push(y);
+    this.clickDrag.push(dragging);
+  }
+
+  private clearCanvas() {
+    this.context
+      .clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.clickX = [];
+    this.clickY = [];
+    this.clickDrag = [];
+  }
+  private clearEventHandler = () => {
+    this.clearCanvas();
+  };
+
+  private releaseEventHandler = () => {
+    this.paint = false;
+    this.redraw();
+  };
+
+  private cancelEventHandler = () => {
+    this.paint = false;
+  };
+  private pressEventHandler = (e: MouseEvent | TouchEvent) => {
+    let mouseX = (e as TouchEvent).changedTouches
+      ? (e as TouchEvent).changedTouches[0].pageX
+      : (e as MouseEvent).pageX;
+    let mouseY = (e as TouchEvent).changedTouches
+      ? (e as TouchEvent).changedTouches[0].pageY
+      : (e as MouseEvent).pageY;
+    mouseX -= this.canvas.offsetLeft;
+    mouseY -= this.canvas.offsetTop;
+
+    this.paint = true;
+    this.addClick(mouseX, mouseY, false);
+    this.redraw();
+  };
+
+  private dragEventHandler = (e: MouseEvent | TouchEvent) => {
+    let mouseX = (e as TouchEvent).changedTouches
+      ? (e as TouchEvent).changedTouches[0].pageX
+      : (e as MouseEvent).pageX;
+    let mouseY = (e as TouchEvent).changedTouches
+      ? (e as TouchEvent).changedTouches[0].pageY
+      : (e as MouseEvent).pageY;
+    mouseX -= this.canvas.offsetLeft;
+    mouseY -= this.canvas.offsetTop;
+
+    if (this.paint) {
+      this.addClick(mouseX, mouseY, true);
+      this.redraw();
+    }
+
+    e.preventDefault();
+  };
 }
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+new DrawingApp();
