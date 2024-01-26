dc982c8daf4fba4afc4db7367d8a70b2f1107e2f
Philipp Kübler <info@pkuebler.de>
2024-01-26 23:25:11 +0100

    copy dist and add plugin

--- build/settings.js
+++ build/settings.js
@@ -1,4 +1,5 @@
 import esbuildPluginTsc from 'esbuild-plugin-tsc';
+import { copy } from 'esbuild-plugin-copy';
 
 export function createBuildSettings(options) {
   return {
@@ -8,6 +9,16 @@ export function createBuildSettings(options) {
     plugins: [
       esbuildPluginTsc({
         force: true
+      }),
+      copy({
+        // this is equal to process.cwd(), which means we use cwd path as base path to resolve `to` path
+        // if not specified, this plugin uses ESBuild.build outdir/outfile options as base path.
+        resolveFrom: 'cwd',
+        assets: {
+          from: ['./src/assets/*'],
+          to: ['./dist'],
+        },
+        watch: true,
       }),
     ],
     ...options
