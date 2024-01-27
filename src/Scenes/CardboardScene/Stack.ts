7a89abb4f961c6bb722a7f5fef4cbc918bce4171
NeroAzure <neroazure@gmx.de>
2024-01-27 15:21:26 +0100

    remove unsued code from stack

--- src/Scenes/CardboardScene/Stack.ts
+++ src/Scenes/CardboardScene/Stack.ts
@@ -1,33 +1,13 @@
-import { Card } from "./Card";
 import { CardInstance } from "./CardInstance";
 import { Hand } from "./Hand";
 
 export class Stack {
-  cards: Array<Card> = [];
   deck: Array<CardInstance> = [];
 
   hand: Hand;
 
   constructor(hand: Hand) {
     this.hand = hand;
-  }
-
-  async load(): Promise<void> {
-    const self = this;
-
-    // create deck instances
-    this.cards.forEach((card, index) => {
-      for (let i = 0; i < 10; i++) {
-        this.deck.push(new CardInstance(card));
-      }
-    });
-
-    // shuffle deck
-    this.shuffle();
-
-    console.log("stack loaded", this.deck.length)
-
-    return ;
   }
 
   shuffle(): void {
