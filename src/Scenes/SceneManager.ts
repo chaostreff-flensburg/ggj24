34f318d43b66bdcb2798a230b4356c4b1301d334
looty <jackal.oak2147@eagereverest.com>
2024-01-27 14:02:56 +0100

    feat: added scenemanager

---
+++ src/Scenes/SceneManager.ts
@@ -0,0 +1,60 @@
+import { Input } from "../Input";
+import { Scene } from "./IScene"
+
+/**
+ * Manages those damn scenes
+ */
+export class SceneManager {
+  // the scene that is visible
+  private activeScene: Scene | null;
+  // last scene that was active
+  private lastScene: Scene | null = null
+  private width: number = 0
+  private height: number = 0
+  // removes the scene automatically after it was hidden
+  private autoDispose: boolean = false
+
+  constructor(initialScene: Scene | null = null) {
+    this.activeScene = initialScene
+  }
+
+  /**
+   * Pushes a scene to the scene list - it also becomes the active scene 
+   * @param 
+   */
+  pushScreen(s: Scene) {
+    if (s) {
+      this.lastScene = this.autoDispose ? null : this.activeScene
+      this.activeScene = s
+    } else {
+      console.error("Cannot add scene. Scene was null")
+    }
+  }
+
+  getActiveScreen(): Scene | null {
+    return this.activeScene
+  }
+
+  getLastScene(): Scene | null {
+    return this.lastScene
+  }
+
+  // TODO pass width and height to resize function of scenes (if implemented)
+  resize(w: number, h: number) {
+    this.width = w
+    this.height = h
+  }
+
+  render(context: CanvasRenderingContext2D) {
+    if (this.activeScene) {
+      this.activeScene.render(context)
+    }
+  }
+
+  update(input: Input) {
+    if (this.activeScene) {
+      this.activeScene.update(input)
+    }
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { DemoScene } from "./Scenes/DemoScene";
 import { Scene } from "./Scenes/IScene";
 import { InputManager } from "./Input";
 import { CardBoardScene } from "./Scenes/CardBoardScene";
+import { SceneManager } from "./Scenes/SceneManager";
 
 class DrawingApp {
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
 
+  private sceneManager: SceneManager;
   private scene: Scene;
 
   private inputManager: InputManager;
@@ -19,27 +21,31 @@ class DrawingApp {
 
     this.scene = new CardBoardScene();
     this.scene.load();
+    this.sceneManager = new SceneManager(this.scene)
+
   }
 
   update() {
-    this.scene.update(this.inputManager.input);
+    //this.scene.update(this.inputManager.input);
+    this.sceneManager.update(this.inputManager.input)
     this.inputManager.update();
   }
 
   render() {
     this.context
       .clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.scene.render(this.context)
+    //this.scene.render(this.context)
+    this.sceneManager.render(this.context)
   }
 
   start() {
     const loop = () => {
       app.update();
       app.render();
-    
+
       window.requestAnimationFrame(loop);
     }
-    
+
     loop();
   }
 }
